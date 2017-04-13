364c985712d782a6007293766da882844f82ee43
Philipp Meisberger <team@pm-codeworks.de>
2017-04-13 17:07:34 +0200

    Added header file information to setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+PAM RFID
+
+Copyright 2014 Philipp Meisberger <team@pm-codeworks.de>
+All rights reserved.
+"""
 
 from setuptools import setup, find_packages
 
@@ -11,7 +19,7 @@ packageVersion = __import__('pamrfid').__version__
 setup(
     name            = 'libpam-rfid',
     version         = packageVersion,
-    description     = 'Pluggable Authentication Module (PAM) for hardware authentication via RFID.',
+    description     = 'Linux Pluggable Authentication Module (PAM) for hardware authentication via RFID.',
     author          = 'Philipp Meisberger',
     author_email    = 'team@pm-codeworks.de',
     url             = 'http://www.pm-codeworks.de/pamrfid.html',
