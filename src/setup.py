91be3cc02bd579801875ffd13af5e8ad21e56c70
Philipp Meisberger <team@pm-codeworks.de>
2019-02-20 09:34:58 +0100

    Simplified retrieving module version

--- src/setup.py
+++ src/setup.py
@@ -8,17 +8,16 @@ Copyright 2014 Philipp Meisberger <team@pm-codeworks.de>
 All rights reserved.
 """
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 import sys
-sys.path.append('./files/')
+sys.path.insert(0, './files/')
 
-## Dynamically get the module version
-packageVersion = __import__('pamrfid').__version__
+import pamrfid
 
 setup(
     name            = 'libpam-rfid',
-    version         = packageVersion,
+    version         = pamrfid.__version__,
     description     = 'Linux Pluggable Authentication Module (PAM) for hardware authentication via RFID.',
     author          = 'Philipp Meisberger',
     author_email    = 'team@pm-codeworks.de',
